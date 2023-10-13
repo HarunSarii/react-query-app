ed015544e272a8c20e0f9b929d6ecca6855638c0
HarunSarii <harunsari14@gmail.com>
2023-10-13 10:32:55 +0300

    feat: clear other files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,9 @@
-import { useQueryClient } from "@tanstack/react-query"
-import { useState } from "react"
-import { getPost } from "./api/posts"
-import { CreatePost } from "./CreatePost"
-import Post from "./Post"
-import { PostListInfinite } from "./PostListInfinite"
-import { PostListPaginated } from "./PostListPaginated"
-import PostsList1 from "./PostsList1"
-import PostsList2 from "./PostsList2"
+import { useState } from "react";
+import PostsList1 from "./PostsList1";
+import PostsList2 from "./PostsList2";
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState(<PostsList1 />)
-  const queryClient = useQueryClient()
-
-  function onHoverPostOneLink() {
-    queryClient.prefetchQuery({
-      queryKey: ["posts", 1],
-      queryFn: () => getPost(1),
-    })
-  }
+  const [currentPage, setCurrentPage] = useState(<PostsList1 />);
 
   return (
     <div>
@@ -27,27 +13,9 @@ export default function App() {
       <button onClick={() => setCurrentPage(<PostsList2 />)}>
         Posts List 2
       </button>
-      <button
-        onMouseEnter={onHoverPostOneLink}
-        onClick={() => setCurrentPage(<Post id={1} />)}
-      >
-        First Post
-      </button>
-      <button
-        onClick={() =>
-          setCurrentPage(<CreatePost setCurrentPage={setCurrentPage} />)
-        }
-      >
-        New Post
-      </button>
-      <button onClick={() => setCurrentPage(<PostListPaginated />)}>
-        Post List Paginated
-      </button>
-      <button onClick={() => setCurrentPage(<PostListInfinite />)}>
-        Post List Infinite
-      </button>
+
       <br />
       {currentPage}
     </div>
-  )
+  );
 }
