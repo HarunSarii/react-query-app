5d2238b9cf01db0d7acd7327161c27fbe8e60058
HarunSarii <harunsari14@gmail.com>
2023-10-13 14:42:41 +0300

    feat: show first post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Post from "./Post";
 import PostsList1 from "./PostsList1";
 import PostsList2 from "./PostsList2";
 
@@ -13,7 +14,9 @@ export default function App() {
       <button onClick={() => setCurrentPage(<PostsList2 />)}>
         Posts List 2
       </button>
-
+      <button onClick={() => setCurrentPage(<Post id={1} />)}>
+        First Post
+      </button>
       <br />
       {currentPage}
     </div>
